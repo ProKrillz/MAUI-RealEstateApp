ede6d4077951596723c4678038e9d0751ee20d9b
ProKrillz <thomasdamkjaer@gmail.com>
2024-01-26 12:19:20 +0100

    Opgave6.2 not done

---
+++ RealEstateApp/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿
+namespace RealEstateApp.ViewModels
+{
+    public class LoginPageViewModel :BaseViewModel
+    {
+        private string _username;
+        public string Username 
+        { 
+            get => _username; 
+            set => SetProperty(ref _username, value); 
+        }
+        private string _password;
+        public string Password 
+        { 
+            get => _password; 
+            set => SetProperty(ref _password, value); 
+        }
+    }
+}
